565ef22a95052ec26bfef4a9cf257d17a8fa44db
Haseeb Ahmed Ansari <haseebahmedansari20@gmail.com>
2024-04-21 23:36:58 -0700

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "55991074fd39f17f2f032a6a499805a4";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
